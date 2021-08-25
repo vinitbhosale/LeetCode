28dc4baec3779f43de6f54b03db6344e5b89cb95
Vinit Bhosale <82602568+vinitbhosale@users.noreply.github.com>
2021-08-25 12:26:59 -0700

    Update 3-longest-substring-without-repeating-characters.java

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,33 +1,33 @@
 class Solution {
     // Brute Force O(n^3) Space Complexity O(min(m, n)) 
-//      public int lengthOfLongestSubstring(String s) {
-//         int n = s.length();
+     public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
 
-//         int res = 0;
-//         for (int i = 0; i < n; i++) {
-//             for (int j = i; j < n; j++) {
-//                 if (checkRepetition(s, i, j)) {
-//                     res = Math.max(res, j - i + 1);
-//                 }
-//             }
-//         }
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (checkRepetition(s, i, j)) {
+                    res = Math.max(res, j - i + 1);
+                }
+            }
+        }
 
-//         return res;
-//     }
+        return res;
+    }
 
-//     private boolean checkRepetition(String s, int start, int end) {
-//         int[] chars = new int[128];
+    private boolean checkRepetition(String s, int start, int end) {
+        int[] chars = new int[128];
 
-//         for (int i = start; i <= end; i++) {
-//             char c = s.charAt(i);
-//             chars[c]++;
-//             if (chars[c] > 1) {
-//                 return false;
-//             }
-//         }
+        for (int i = start; i <= end; i++) {
+            char c = s.charAt(i);
+            chars[c]++;
+            if (chars[c] > 1) {
+                return false;
+            }
+        }
 
-//         return true;
-//     }
+        return true;
+    }
     
  //----------------------------------------Optimize---------------------------------------------//
     
