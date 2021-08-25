3d10414f9a648ac0a31a8ac53a3999fe93272329
Vinit Bhosale <82602568+vinitbhosale@users.noreply.github.com>
2021-08-25 12:06:28 -0700

    Update 42-trapping-rain-water.java

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,32 +1,32 @@
 class Solution {
     // Brute Force Time Complexity O(N^2) and Space complexity O(1)
-//     public int trap(int[] height) {
+    public int trap(int[] height) {
         
-//         if(height.length < 2  || height == null)
-//             return 0;
+        if(height.length < 2  || height == null)
+            return 0;
         
-//         int waterCount = 0;
+        int waterCount = 0;
         
-//         // looping from second position as ist position will level will never have water till one position before last
-//         for(int i = 1; i < height.length - 1; i++){
-//             int leftMax = 0;
-//             int rightMax = 0;
+        // looping from second position as ist position will level will never have water till one position before last
+        for(int i = 1; i < height.length - 1; i++){
+            int leftMax = 0;
+            int rightMax = 0;
             
-//             // leftMax for ith position
-//             for(int j = i; j >=0; j--){
-//                 leftMax = Math.max(leftMax, height[j]);
-//             }
-//             // rightMax for ith position
-//              for(int j = i; j < height.length; j++){
-//                 rightMax = Math.max(rightMax, height[j]);
-//             }
+            // leftMax for ith position
+            for(int j = i; j >=0; j--){
+                leftMax = Math.max(leftMax, height[j]);
+            }
+            // rightMax for ith position
+             for(int j = i; j < height.length; j++){
+                rightMax = Math.max(rightMax, height[j]);
+            }
             
-//             // calculate the water count by taking min from leftMax and rightMax and subtracting it with the height at ith position
-//             waterCount += Math.min(leftMax, rightMax) - height[i];
-//         }
+            // calculate the water count by taking min from leftMax and rightMax and subtracting it with the height at ith position
+            waterCount += Math.min(leftMax, rightMax) - height[i];
+        }
         
-//         return waterCount;
-//     }
+        return waterCount;
+    }
 //------------------------------------------------------------------------------------------------------------------------//
     //Time Complexity O(N) and Space complexity O(1)
     public int trap(int[] height) {
