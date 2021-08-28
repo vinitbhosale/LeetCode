13aff780513876230cd6cf47532fad91c21a7c84
Vinit Bhosale <82602568+vinitbhosale@users.noreply.github.com>
2021-08-28 13:42:50 -0700

    Time: 25 ms (49.06%), Space: 89.2 MB (17.00%) - LeetHub

---
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.java
@@ -0,0 +1,52 @@
+class RandomizedSet {
+
+    private Map<Integer, Integer> map;
+    private List<Integer> list;
+    private Random random;
+    
+    /** Initialize your data structure here. */
+    public RandomizedSet() {
+        map = new HashMap<>();
+        list = new ArrayList<>();
+        random = new Random();
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        if(map.containsKey(val)) return false;
+        map.put(val, list.size());
+        list.add(val);
+        return true;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        if(!map.containsKey(val)) return false;
+        
+        // Make the second last element of list to last update the index value for the second in map
+        int index = map.get(val);
+        int lastElement = list.get(list.size() - 1);
+        list.set(index, lastElement);
+        map.put(lastElement, index);
+        
+        map.remove(val);
+        list.remove(list.size() - 1);
+        
+        return true;
+    
+    }
+    
+    /** Get a random element from the set. */
+    public int getRandom() {
+        int index = random.nextInt(list.size());
+        return list.get(index);
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
