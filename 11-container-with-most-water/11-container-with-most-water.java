ce4e8d1c6e0059fc386175c431743fe4fc04c075
Vinit Bhosale <82602568+vinitbhosale@users.noreply.github.com>
2021-08-29 10:35:42 -0700

    Update 11-container-with-most-water.java

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -1,12 +1,12 @@
 class Solution {
      //Brute Force Time complexity O(N^2) space complexity O(1)
-    // public int maxArea(int[] height) {
-    //     int maxarea = 0;
-    //     for (int i = 0; i < height.length; i++)
-    //         for (int j = i + 1; j < height.length; j++)
-    //             maxarea = Math.max(maxarea, Math.min(height[i], height[j]) * (j - i));
-    //     return maxarea;
-    // }
+    public int maxArea(int[] height) {
+        int maxarea = 0;
+        for (int i = 0; i < height.length; i++)
+            for (int j = i + 1; j < height.length; j++)
+                maxarea = Math.max(maxarea, Math.min(height[i], height[j]) * (j - i));
+        return maxarea;
+    }
 
 //----------------------------------Optimize-----------------------------------------------------//
     
