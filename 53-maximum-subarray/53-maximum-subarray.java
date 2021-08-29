44e373a44b94ec1b71a8e1094056de8bb667e408
Vinit Bhosale <82602568+vinitbhosale@users.noreply.github.com>
2021-08-28 23:09:43 -0700

    Time: 0 ms (100.00%), Space: 39 MB (54.41%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -0,0 +1,34 @@
+class Solution {
+      // Brute Force Time Complexity O(N^2) and Space Complexity O(1)
+//     public int maxSubArray(int[] nums) {
+//         int maxSubarray = Integer.MIN_VALUE;
+//         for (int i = 0; i < nums.length; i++) {
+//             int currentSubarray = 0;
+//             for (int j = i; j < nums.length; j++) {
+//                 currentSubarray += nums[j];
+//                 maxSubarray = Math.max(maxSubarray, currentSubarray);
+//             }
+//         }
+        
+//         return maxSubarray;
+//     }
+    
+//------------------------------Optimize----------------------------------------//
+    
+    //Time Complexity O(N) and Space Complexity O(1) 
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int currentMax = max;
+        
+        for(int i = 1; i< nums.length; i++){
+            if(nums[i]+currentMax > nums[i]){
+                currentMax = nums[i]+currentMax;
+                max = Math.max(max, currentMax);
+            }else{
+                currentMax = nums[i];
+                max = Math.max(max, currentMax);
+            }
+        }
+        return max;
+    }
+}
