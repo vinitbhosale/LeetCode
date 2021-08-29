f9f56d3e41d7191d9d342b47b63b11b7212045bf
Vinit Bhosale <82602568+vinitbhosale@users.noreply.github.com>
2021-08-29 11:13:52 -0700

    Update 1209-remove-all-adjacent-duplicates-in-string-ii.java

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,21 +1,21 @@
 class Solution {
     // Brute Force Time Complexity O(N^2/k) and Space Complexity O(1)
-//     public String removeDuplicates(String s, int k) {
-//     StringBuilder sb = new StringBuilder(s);
-//     int length = -1;
-//     while (length != sb.length()) {
-//         length = sb.length();
-//         for (int i = 0, count = 1; i < sb.length(); ++i) {
-//             if (i == 0 || sb.charAt(i) != sb.charAt(i - 1)) {
-//                 count = 1;
-//             } else if (++count == k) {
-//                 sb.delete(i - k + 1, i + 1);
-//                 break;
-//             }
-//         }
-//     }
-//     return sb.toString();
-// }
+    public String removeDuplicates(String s, int k) {
+    StringBuilder sb = new StringBuilder(s);
+    int length = -1;
+    while (length != sb.length()) {
+        length = sb.length();
+        for (int i = 0, count = 1; i < sb.length(); ++i) {
+            if (i == 0 || sb.charAt(i) != sb.charAt(i - 1)) {
+                count = 1;
+            } else if (++count == k) {
+                sb.delete(i - k + 1, i + 1);
+                break;
+            }
+        }
+    }
+    return sb.toString();
+}
     
 //------------------------------optimize------------------------------------------------------//
     
