97d3d3c3ea42f5bd565576a6f885ede98c31b226
Vinit Bhosale <82602568+vinitbhosale@users.noreply.github.com>
2021-09-12 10:52:42 -0700

    Time: 0 ms (100.00%), Space: 38.7 MB (51.05%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    // Time complexity O(N) and Space Complexity O(1)
+    public int arraySign(int[] nums) {
+        int sign = 1;
+        
+        for(int n : nums){
+            if(n == 0)
+                return 0;
+            if(n < 0)
+                sign = -sign;
+        }
+        
+        return sign;
+    }
+}
