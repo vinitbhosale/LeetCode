9c8ff9ad54b8fec2515530e5389db3d8f88a8c57
Vinit Bhosale <82602568+vinitbhosale@users.noreply.github.com>
2021-09-12 22:12:05 -0700

    Time: 3 ms (86.85%), Space: 39.6 MB (61.61%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.java
@@ -0,0 +1,71 @@
+class Solution {
+    //dfs 
+//     public boolean canFinish(int numCourses, int[][] prerequisites) {
+//         if(prerequisites.length == 0)return true;
+        
+//         List<List<Integer>> graph = new ArrayList<>();
+        
+//         for(int i = 0; i < numCourses; i++)
+//             graph.add(new ArrayList<Integer>());
+        
+//         for(int[] pre : prerequisites)
+//             graph.get(pre[1]).add(pre[0]);
+        
+//         int[] visited = new int[numCourses];
+        
+//         for(int i = 0; i < numCourses; i++)
+//             if(!dfs(graph, visited, i)) return false;
+        
+//         return true;
+//     }
+    
+//     private boolean dfs(List<List<Integer>> graph, int[] visited, int v){
+//         visited[v] = 1;
+        
+//         List<Integer> adj = graph.get(v);
+        
+//         for(int i = 0; i < adj.size(); i++){
+//             int eligibleCourse = adj.get(i);
+//             if(visited[eligibleCourse] == 1) return false;
+            
+//             if(visited[eligibleCourse] == 0)
+//                 if(!dfs(graph, visited, eligibleCourse)) return false;
+//         }
+        
+//         visited[v] = 2;
+        
+//         return true;
+//     }
+    
+//------------------------------------------------------------------------------------------//   
+    
+    // BFS 
+    public boolean canFinish(int numCourses, int[][] prerequisites){
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i = 0; i < numCourses; i++)
+            graph.add(new ArrayList<Integer>());
+        
+        int[] inDegree = new int[numCourses];
+        for(int[] pre : prerequisites){
+            graph.get(pre[1]).add(pre[0]);
+            inDegree[pre[0]]++;
+        }
+            
+            
+        
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i < numCourses; i++)
+            if(inDegree[i] == 0) queue.add(i);
+        
+        while(!queue.isEmpty()){
+            int current = queue.remove();
+            for(int courses : graph.get(current)){
+                inDegree[courses]--;
+                if(inDegree[courses] == 0) queue.add(courses);
+            }
+            numCourses--;
+        }
+        
+        return numCourses == 0;
+    }
+}
