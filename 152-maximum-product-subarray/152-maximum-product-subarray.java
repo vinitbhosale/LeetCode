c9e576ee5aac385f7613869fcb83ec649687daad
Vinit Bhosale <82602568+vinitbhosale@users.noreply.github.com>
2021-09-30 21:38:52 -0700

    Update 152-maximum-product-subarray.java

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -1,21 +1,21 @@
 class Solution {
     // Brute Force
     // Time Complexity O(n^2) and Space Complexity O(1)
-//      public int maxProduct(int[] nums) {
-//         if (nums.length == 0) return 0;
+     public int maxProduct(int[] nums) {
+        if (nums.length == 0) return 0;
 
-//         int result = nums[0];
+        int result = nums[0];
 
-//         for (int i = 0; i < nums.length; i++) {
-//             int accu = 1;
-//             for (int j = i; j < nums.length; j++) {
-//                 accu *= nums[j];
-//                 result = Math.max(result, accu);
-//             }
-//         }
+        for (int i = 0; i < nums.length; i++) {
+            int accu = 1;
+            for (int j = i; j < nums.length; j++) {
+                accu *= nums[j];
+                result = Math.max(result, accu);
+            }
+        }
 
-//         return result;
-//     }
+        return result;
+    }
     
 //--------------------------------------OPTIMIZE-----------------------------------//
     // Dp Solution
