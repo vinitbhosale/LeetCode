68706ba5a075201dfaec01ddd9e5e7c784c172e0
Vinit Bhosale <82602568+vinitbhosale@users.noreply.github.com>
2021-10-11 21:33:39 -0700

    Update 169-majority-element.java

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,43 +1,43 @@
 class Solution {
 //--------------------------------Brute Force---------------------------------------------//
-//     // Time COmplexity O(N^2) and Space COmplexity O(1)
-//     public int majorityElement(int[] nums) {
-//         int majorityCount = nums.length/2;
+    // Time COmplexity O(N^2) and Space COmplexity O(1)
+    public int majorityElement(int[] nums) {
+        int majorityCount = nums.length/2;
 
-//         for (int num : nums) {
-//             int count = 0;
-//             for (int elem : nums) {
-//                 if (elem == num) {
-//                     count += 1;
-//                 }
-//             }
+        for (int num : nums) {
+            int count = 0;
+            for (int elem : nums) {
+                if (elem == num) {
+                    count += 1;
+                }
+            }
 
-//             if (count > majorityCount) {
-//                 return num;
-//             }
+            if (count > majorityCount) {
+                return num;
+            }
 
-//         }
+        }
 
-//         return -1;    
-//     }
+        return -1;    
+    }
     
-// //---------------------------------Optimization to O(N)-----------------------------------//
-//     // Time Complexity O(N) adn Space Complexity (N)
-//     public int majorityElement(int[] nums) {
-//         HashMap<Integer, Integer> nums_count = new HashMap<>();
+//---------------------------------Optimization to O(N)-----------------------------------//
+    // Time Complexity O(N) adn Space Complexity (N)
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> nums_count = new HashMap<>();
         
-//         for(int n : nums){
-//             int count = nums_count.containsKey(n) ? nums_count.get(n) : 0;
-//             nums_count.put(n, count + 1);
-//         }
-//         Map.Entry<Integer, Integer> max_element = null;
-//         for(Map.Entry<Integer, Integer> entry : nums_count.entrySet()){
-//             if(max_element == null || entry.getValue() > max_element.getValue()){
-//                 max_element = entry;
-//             }
-//         }
-//         return max_element.getKey();
-//     }
+        for(int n : nums){
+            int count = nums_count.containsKey(n) ? nums_count.get(n) : 0;
+            nums_count.put(n, count + 1);
+        }
+        Map.Entry<Integer, Integer> max_element = null;
+        for(Map.Entry<Integer, Integer> entry : nums_count.entrySet()){
+            if(max_element == null || entry.getValue() > max_element.getValue()){
+                max_element = entry;
+            }
+        }
+        return max_element.getKey();
+    }
     
 //---------------------------------Boyer-Moore voting Algo-------------------------------//
     //Time Complexity O(N) and Space COmplexity O(1)
